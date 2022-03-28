dcdc5259d4b2ebad5ec086640222e43ba725e267
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2022-03-29 00:33:18 +0530

    changed the background image

--- flappy.py
+++ flappy.py
@@ -7,7 +7,7 @@ import neat
 pygame.font.init()  # init font
 
 WIN_WIDTH = 600
-WIN_HEIGHT = 800
+WIN_HEIGHT = 600
 FLOOR = 700
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
 END_FONT = pygame.font.SysFont("comicsans", 70)
@@ -17,7 +17,7 @@ WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Obstacle Game")
 
 pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")).convert_alpha())
-bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","sp.png")).convert_alpha(), (600, 900))
+bg_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","landscape_day.png")).convert_alpha(), (600, 600))
 bird_images = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pla.png")))
 base_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","basee.png")),(270,100))
 
