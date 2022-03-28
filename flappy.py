5e8f4a7c3e9f30a6a14168a7742dd7f5e30816b4
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2022-03-29 02:20:00 +0530

    window size chnaged to 1080*1080

--- flappy.py
+++ flappy.py
@@ -6,8 +6,8 @@ import neat
 
 pygame.font.init()  # init font
 
-WIN_WIDTH = 600
-WIN_HEIGHT = 600
+WIN_WIDTH = 1080
+WIN_HEIGHT = 1080
 FLOOR = 700
 STAT_FONT = pygame.font.SysFont("comicsans", 50)
 END_FONT = pygame.font.SysFont("comicsans", 70)
@@ -17,8 +17,8 @@ WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 pygame.display.set_caption("Obstacle Game")
 
 pipe_img = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")).convert_alpha())
-bg_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("imgs","landscape_day.png")).convert_alpha(), (600, 600))
-bg_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("imgs","landscape_night.png")).convert_alpha(), (600, 600))
+bg_img_1 = pygame.transform.scale(pygame.image.load(os.path.join("imgs","landscape_day.png")).convert_alpha(), (700, 700))
+bg_img_2 = pygame.transform.scale(pygame.image.load(os.path.join("imgs","landscape_night.png")).convert_alpha(), (1080, 1080))
 bird_images = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","gbird.png")))
 base_img = pygame.transform.scale(pygame.image.load(os.path.join("imgs","basee.png")),(270,100))
 
